2fe2e43ae4aee62eef8b313096d2186715f57b6d
root <root@sdw2648.vps.phps.kr>
2014-08-31 15:15:53 +0900

    dev

---
+++ kucinema/models.py
@@ -0,0 +1,39 @@
+# -*- coding:utf-8 -*-
+from django.db import models
+import watson
+
+
+class Movie(models.Model):
+    title = models.TextField()
+    """
+    genre
+    running_time
+    age_for
+    director
+    pic_url
+    """
+
+    class Meta:
+        app_label = 'kucinema'
+
+    def __unicode__(self):
+        return self.title
+
+
+class Schedule(models.Model):
+    schedule = models.DateTimeField()
+    movie = models.ForeignKey(Movie)
+
+    THEATER_CHOCIES = (
+        ('trap', ''),
+        ('theque', ''),
+    )
+
+    theater = models.CharField(length=1, choices=THEATER_CHOCIES)
+
+    class Meta:
+        app_label = 'kucinema'
+
+    def __unicode__(self):
+        return '%s - %s' % (movie.title, self.schedule)
+
---
+++ kucinema/views.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+kucinema_trap = 'http://www.kucinetrap.kr/'
+kucinema_theque = 'http://www.kucine.kr/'
+schedule_postfix = 'async/weeklyschedule.php'
+
+def parse(url):
+    params = {
+        'date': '20140831'
+    }
+    r = requests.post(url + schedule_postfix, params=params)
+
+    print r.text
+
+parse(kucinema_trap)
+parse(kucinema_theque)
